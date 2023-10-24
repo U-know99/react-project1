b9d220c9d4dc3dd2905215755d319a2b9de930bc
U-know99 <125248719+U-know99@users.noreply.github.com>
2023-10-25 01:59:58 +0900

    coin

--- src/App.js
+++ src/App.js
@@ -1,39 +1,62 @@
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
 
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
+  const [selectedCoin, setSelectedCoin] = useState("BTC");
+  const [dollar, setDollar] = useState();
 
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-  console.log(toDos);
+  function onSelect(event) {
+    const newSelectedCoin = event.target.value;
+    setSelectedCoin(newSelectedCoin);
+
+    // 선택한 코인을 바꾸면서 바로 계산을 수행
+    convert({ target: { value: dollar } });
+  }
+
+  function convert(event) {
+    const selectedCoinInfo = coins.find((coin) => coin.symbol === selectedCoin);
+    const usdAmount = event.target.value;
+    const converted = (usdAmount / selectedCoinInfo.quotes.USD.price).toFixed(
+      3
+    );
+
+    setDollar(converted);
+  }
+  // 선택한 코인의 심볼
+
   return (
     <div>
-      <h1 className={styles.title}>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>
+        The Amazing Coin Converter!
+        {loading ? "" : ` ( with ${coins.length}coins )`}
+      </h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onSelect}>
+          {coins.map((coin) => (
+            <option key={coin.id} value={coin.symbol}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}USD{" "}
+            </option>
+          ))}
+        </select>
+      )}
+      <div>
+        <h2>You want convert USD to {selectedCoin} </h2>
+        If you have <input onChange={convert} type="number" /> USD
+      </div>
+      <div>You can buy {dollar}</div>
     </div>
   );
 }
